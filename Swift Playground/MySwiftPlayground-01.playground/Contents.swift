5c293c9f345de3d28bc9f667c1a3a29e89fd40e2
Rahul Singha Roy <40661447+Rsroy@users.noreply.github.com>
2022-10-30 11:23:43 +0530

    Swift coding challenge 02

--- Swift Playground/MySwiftPlayground-01.playground/Contents.swift
+++ Swift Playground/MySwiftPlayground-01.playground/Contents.swift
@@ -11,5 +11,41 @@ import UIKit
  
  */
 
+// Type - 1
+func reverseString(_ inputString: String) -> String {
+    let stringArray = Array(inputString)
+    var reverseString = ""
+    for word in stringArray.reversed() {
+        reverseString.append(word)
+    }
+
+    return reverseString
+}
+
+// Type - 2
+func reverseString2(_ inputString: String) -> String {
+    let stringArray = Array(inputString)
+    var reverseString = ""
+    
+    for index in stride(from: stringArray.count, to: 0, by: -1) {
+        reverseString.append(stringArray[index - 1])
+    }
+
+    return reverseString
+}
+
+let testString = "I love Codecademy!"
+let expectation = "!ymedacedoC evol I"
+
+//
+print(reverseString2(testString))
+
+if reverseString2(testString) == expectation {
+    print("Success..")
+} else {
+    print("Failure!!")
+}
+
+
 
 
