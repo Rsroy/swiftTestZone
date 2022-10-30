f3f668b5a9bda8408b2e293b4295c91a54c63efa
Rahul Singha Roy <40661447+Rsroy@users.noreply.github.com>
2022-10-30 11:06:44 +0530

    New playground file added

--- Swift Playground/MySwiftPlayground-01.playground/Contents.swift
+++ Swift Playground/MySwiftPlayground-01.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Swift coding challenges - https://www.codecademy.com/
+
+/*
+
+ Reverse a string
+ Come up with a function that takes an input string and reverses its order. For example, "I love Codecademy!" should return "!ymedacedoC evol I."
+
+ For an added challenge, switch capitalization so that each new word is capitalized. So, "I love Codecademy" outputs as "Ymedacedoc Evol I."
+ 
+ */
+
+
+
